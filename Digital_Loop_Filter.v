1c4e5e82f145cdb49609161066931ef1a1f307ec
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-12-22 17:01:46 +0800

    Add files via upload

--- Digital_Loop_Filter.v
+++ Digital_Loop_Filter.v
@@ -20,48 +20,54 @@ parameter coeff_decimal_width = 18;
 parameter coeff_width = coeff_int_width + coeff_decimal_width;
 
 
-parameter signed [coeff_width - 1 : 0] b0 = 20'b00_0000_0001_0100_0000_00;
-parameter signed [coeff_width - 1 : 0] b1 = 20'b00_0000_0001_0100_1100_01;
-parameter signed [coeff_width - 1 : 0] b2 = 20'b11_1111_1110_1101_1000_10;
-parameter signed [coeff_width - 1 : 0] b3 = 20'b11_1111_1110_1100_1100_00;
+parameter signed [coeff_width - 1 : 0] b0 = 20'b00_0000_0010_1000_0000_00; //0.0097690
+parameter signed [coeff_width - 1 : 0] b1 = 20'b00_0000_0010_1001_1000_11; //0.0101456
+parameter signed [coeff_width - 1 : 0] b2 = 20'b11_1111_1101_1011_0001_01; //-0.0090159
+parameter signed [coeff_width - 1 : 0] b3 = 20'b11_1111_1101_1001_1000_10; //-0.0093925
   
-parameter signed [coeff_width - 1 : 0] a1 = 20'b01_1010_0100_0101_1010_00;
-parameter signed [coeff_width - 1 : 0] a2 = 20'b00_1011_0100_0110_1011_11;
-parameter signed [coeff_width - 1 : 0] a3 = 20'b11_1110_1111_1111_0000_11;
+parameter signed [coeff_width - 1 : 0] a1 = 20'b10_0101_1011_1010_0110_00; //-1.64201
+parameter signed [coeff_width - 1 : 0] a2 = 20'b00_1011_0100_0110_1011_11; //0.70477
+parameter signed [coeff_width - 1 : 0] a3 = 20'b11_1110_1111_1111_0000_11; //-0.06273
 
 
 input clk;
 input rstn;
-signed input wire [inout_width - 1 : 0] master_in;
-signed output reg [inout_width - 1 : 0] slave_out;
+input lead;
+input wire [inout_width - 1 : 0] master_in;
+output reg [inout_width - 1 : 0] slave_out;
 
 
 //register
-signed reg [inout_width - 1 : 0] in_delay1;
-signed reg [inout_width - 1 : 0] in_delay2;
-signed reg [inout_width - 1 : 0] in_delay3; 
+signed wire [inout_width : 0] in_temp;
+signed reg [inout_width : 0] in_delay1;
+signed reg [inout_width : 0] in_delay2;
+signed reg [inout_width : 0] in_delay3; 
 			
-signed reg [inout_width - 1 : 0] out_delay1;
-signed reg [inout_width - 1 : 0] out_delay2;
-signed reg [inout_width - 1 : 0] out_delay3;
+signed reg [inout_width : 0] out_delay1;
+signed reg [inout_width : 0] out_delay2;
+signed reg [inout_width : 0] out_delay3;
 
 //wire
-signed wire [inout_width + coeff_width - 1 : 0] in1;  
-signed wire [inout_width + coeff_width - 1 : 0] in2; 
-signed wire [inout_width + coeff_width - 1 : 0] in3; 
+signed wire [inout_width + coeff_width : 0] in1;  
+signed wire [inout_width + coeff_width : 0] in2; 
+signed wire [inout_width + coeff_width : 0] in3; 
 			  
-signed wire [inout_width + coeff_width - 1 : 0] out1;  
-signed wire [inout_width + coeff_width - 1 : 0] out2; 
-signed wire [inout_width + coeff_width - 1 : 0] out3;
+signed wire [inout_width + coeff_width : 0] out1;  
+signed wire [inout_width + coeff_width : 0] out2; 
+signed wire [inout_width + coeff_width : 0] out3;
 
-signed wire [inout_width + coeff_width + 2 : 0] out_sum;
+signed wire [inout_width + coeff_width + 3 : 0] out_sum;
  
+//change to signed
+always @ (*) begin
+	if(lead) in_temp = {1'b0, master_in};
+	else begin in_temp = {1'b1, ~master_in} + 1'b1;
+end
 
 //numerator
 assign in1 = b1 * in_delay1;
 assign in2 = b2 * in_delay2;
 assign in3 = b3 * in_delay3;
-
 
 //denominator
 assign out1 = a1 * out_delay1;
@@ -70,20 +76,20 @@ assign out3 = a3 * out_delay3;
 
 always @(posedge clk) begin
 	if (!rstn) begin
-		in_delay1 <= 7'b0;
-		in_delay2 <= 7'b0;
-		in_delay3 <= 7'b0;
+		in_delay1 <= 9'b0;
+		in_delay2 <= 9'b0;
+		in_delay3 <= 9'b0;
 		
-		out_delay1 <= 7'b0;
-		out_delay2 <= 7'b0;
-		out_delay3 <= 7'b0;
+		out_delay1 <= 9'b0;
+		out_delay2 <= 9'b0;
+		out_delay3 <= 9'b0;
 	end
 	else begin
-		in_delay1 <= master_in;
+		in_delay1 <= in_temp;
 		in_delay2 <= in_delay1;
 		in_delay3 <= in_delay2;
 		
-		out_delay1 <= output_int;
+		out_delay1 <= out_temp;
 		out_delay2 <= out_delay1;
 		out_delay3 <= out_delay2;
     end
@@ -91,7 +97,11 @@ end
 
 //output
 assign out_sum = b0 + in1 + in2 + in3 - out1 - out2 - out3;
-assign slave_out = out_sum[coeff_decimal_width + coeff_width - 1 : coeff_decimal_width];
-
+assign out_temp = out_sum[coeff_decimal_width + inout_width  : coeff_decimal_width];
+assign slave_out = out_temp[7:0]
 
 endmodule
+
+
+
+
