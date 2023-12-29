fd4f779e1f802558dff67c489e06075bbc537c29
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-12-29 17:58:36 +0800

    Add files via upload

--- Digital_Loop_Filter.v
+++ Digital_Loop_Filter.v
@@ -104,8 +104,11 @@ end
 
 
 //output
+wire [inout_width - 1 : 0] out_temp;
+
 assign out_sum = in0 + in1 + in2 + in3 - out1 - out2 - out3;
-assign slave_out = {~out_sum[coeff_decimal_width + inout_width - 1], out_sum[coeff_decimal_width + inout_width - 2 : coeff_decimal_width]};
+assign out_temp = {out_sum[coeff_decimal_width + inout_width - 1 : coeff_decimal_width]};
+assign slave_out = {~out_temp[inout_width - 1], out_temp[inout_width - 1 : 0]};
 
 
 endmodule
